122658f5b2499bc182a28534b6360cc6ae650c7e
Scott Cowe <scott.t.cowe@gmail.com>
2026-08-25 17:45:53 +0100

    sdffsddf

--- src/gs.rs
+++ src/gs.rs
@@ -57,12 +57,12 @@ pub fn update_target_deg(around: Option<f32>, updown: Option<f32>) {
 
 pub fn update_target_lat_long(lat: f32, long: f32) {
     unsafe {
-        TARGET_DIST = dist(SELF_LAT, SELF_LONG, lat, long);
-        TARGET_POSE_DEG[0] = (bearing(SELF_LAT, SELF_LONG, lat, long).to_degrees() + 360.) % 360.;
+        // TARGET_DIST = dist(SELF_LAT, SELF_LONG, lat, long);
+        TARGET_POSE_DEG[0] = 90.;//((bearing(SELF_LAT, SELF_LONG, lat, long).to_degrees() + 360.) % 360.);
         // writeln!(get_serial(), "angle {}", TARGET_POSE_DEG[0]);
-        // use cortex_m_semihosting::hprintln;
-        // hprintln!("deg theta: {}", TARGET_POSE_DEG[0]);
-        // hprintln!("dist: {}", TARGET_DIST);
+        use cortex_m_semihosting::hprintln;
+        hprintln!("deg theta: {}", TARGET_POSE_DEG[0]);
+        hprintln!("dist: {}", TARGET_DIST);
     }
 }
 
@@ -119,7 +119,7 @@ const X_STEPS_PER_REV: f32 = 2080.;
 const Y_STEPS_PER_REV: f32 = 770. * 11.; 
 const X_DEG_PER_STEP: f32 = 360.0 / X_STEPS_PER_REV;
 const Y_DEG_PER_STEP: f32 = 360.0 / Y_STEPS_PER_REV;
-const MAX_FREQ_HZ: f32 = 600.0;
+const MAX_FREQ_HZ: f32 = 1100.0;
 const DEADBAND_DEG: f32 = 0.7;
 
 #[interrupt]
@@ -134,6 +134,8 @@ fn TIM5() {
 // called in ISR every 10ms
 fn motor_tick() {
     unsafe {
+        writeln!(get_serial(), "Target: {}°", TARGET_POSE_DEG[0]).unwrap();
+
         let x_pwm = X_PWM_CHANNEL.as_mut().unwrap();
         let x_man = X_PWM_MANAGER.as_mut().unwrap();
         let x_dir = X_DIR_PIN.as_mut().unwrap();
